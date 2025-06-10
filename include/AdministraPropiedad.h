61befd51607f3b52467173b156472bac567cc77c
Martin <martinpedragosa730@gmail.com>
2025-06-10 16:00:00 -0300

    AdministraPropiedad.h

--- include/AdministraPropiedad.h
+++ include/AdministraPropiedad.h
@@ -1,6 +1,9 @@
 #ifndef ADMINISTRAPROPIEDAD_H
 #define ADMINISTRAPROPIEDAD_H
 #include "DTFecha.h"
+#include "Inmueble.h"
+#include "Inmobiliaria.h"
+#include "Publicacion.h"
 
 class AdministraPropiedad {
     private:
@@ -8,6 +11,10 @@ class AdministraPropiedad {
 
     public:
         AdministraPropiedad(DTFecha* fecha);
+    Inmueble getInmueble();
+    Inmobiliaria getInmobiliaria();
+    void DarBajaPubicaciones(); 
+    void borrarOublicaciones(Publicacion p); 
         ~AdministraPropiedad();
 };
 
