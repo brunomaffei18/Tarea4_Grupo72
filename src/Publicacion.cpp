6d88379e6e7da7ffe7e5395f42f244f6a381d39a
Ariel Ignacio Falcon Marroni - ENS <ariel.falcon@pcunix136.fing.edu.uy>
2025-06-12 14:31:07 -0300

    Publicacion.cpp

--- src/Publicacion.cpp
+++ src/Publicacion.cpp
@@ -1,27 +1,30 @@
-#ifndef PUBLICACION_H
-#define PUBLICACION_H
-#include "Publicacion.h"
 #include "DTFecha.h"
-#include "TipoPublicacion.h"
-#include <string>
-#include "AdministraPropiedad.h"
 #include "../include/Publicacion.h"
+#include "../include/AdministraPropiedad.h" 
 
 Publicacion::Publicacion(int codigo, DTFecha* fecha, TipoPublicacion tipo, std::string texto, float precio, bool activa)
     : codigo(codigo), fecha(fecha), tipo(tipo), texto(texto), precio(precio), activa(activa), adminProp(nullptr) {}
     
-int Publicacion::getCodigo();{return this.}
-DTFecha* getFecha();
-std::string getTexto();
-std::string getPrecio();
-std::string getInmobiliaria();
-AdministraPropiedad* getAdministra();
-bool Publicacion::esActiva();{return this.}
-bool verificarInmobiiaria(int codigoInmbo,int codigoInmueble,std::string nickNameInmobiliaria,std::string nickNameInmob);
-~Publicacion();
+int Publicacion::getCodigo(){return this->codigo;}
+
+DTFecha* Publicacion::getFecha() {return this->fecha;}
+
+std::string Publicacion::getTexto(){return this->texto;}
+
+float Publicacion::getPrecio(){return this->precio;}
+
+//std::string Publicacion::getInmobiliaria(); No figura en nuestro diagrama
+
+AdministraPropiedad* Publicacion::getAdministra(){this->adminProp;}
+
+bool Publicacion::esActiva(){return this->activa;}
+
+bool Publicacion::verificarInmobiiaria(int codigoInmbo,int codigoInmueble,std::string nickNameInmobiliaria,std::string nickNameInmob){
+    return ((codigoInmbo==codigoInmueble) && (nickNameInmob==nickNameInmobiliaria));
+}
+
+Publicacion::~Publicacion(){
+    delete this->fecha;
+}
 
 
-
-
-
-#endif
