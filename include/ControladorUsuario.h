839cffe4b6662357662dc8510dac3ad05fb96ebc
Martin <martinpedragosa730@gmail.com>
2025-06-12 16:43:56 -0300

    Controladores

---
+++ include/ControladorUsuario.h
@@ -0,0 +1,37 @@
+#ifndef CONTROLADORUSUARIO_H 
+#define CONTROLADORUSUARIO_H
+
+#include <string>
+#include <set>
+#include "DTUsuario.h"
+#include "Propietario.h"
+#include "Cliente.h"
+#include "Inmobiliaria.h"
+
+
+
+class ControladorUsuario
+{
+private:
+    static ControladorUsuario* Intancia;
+    ControladorUsuario();
+public:
+   static ControladorUsuario* getInstancia();
+     bool altaCliente(std::string nickname, std::string contrasena, std::string nombre, std::string email,std::string apellido, std::string documento);
+        bool altaInmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string direccion,std::string url, std::string telefono);
+        bool altaPropietario(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string cuentaBancaria,std::string telefono);
+        std::set<DTUsuario*> ListarInmobiliarias();
+        std::set<DTUsuario*> ListarPropietarios();
+        std::set<DTUsuario*> ListarClientes();
+        
+        
+    ~ControladorUsuario();
+};
+
+
+
+
+
+
+
+#endif
