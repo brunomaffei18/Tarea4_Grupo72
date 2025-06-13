9b5dbfa72eef4917be38c747a8ce2b7901364d30
Ariel Ignacio Falcon Marroni - ENS <ariel.falcon@pcunix135.fing.edu.uy>
2025-06-13 14:52:33 -0300

    Inmobiliaria

--- include/Inmobiliaria.h
+++ include/Inmobiliaria.h
@@ -1,21 +1,25 @@
 #ifndef INMOBILIARIA_H
 #define INMOBILIARIA_H
-#include "Usuario.h"
+
 #include <string>
+#include <set>
+#include "Usuario.h"
 #include "Subscriptor.h"
+#include "DTNotificacion.h"
 
 class Inmobiliaria : public Usuario {
 private:
     std::string direccion;
     std::string url;
     std::string telefono;
+    std::set<Subscriptor*> suscriptores;
 
 public:
-    Inmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string direccion, std::string url, std::string telefono);
-
-    void agregarSubscriptor(Subscriptor s);
-    void eliminarSubscriptor(Subscriptor s);
-    void notificar(Publicacion p);
+    Inmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email,
+        std::string direccion, std::string telefono, std::string url);
+    void agregarSubscriptor(Subscriptor* s);
+    void eliminarSubscriptor(Subscriptor* s);
+    void notificar(const DTNotificacion& n);
 
     ~Inmobiliaria();
 };
---
+++ src/Inmobiliaria.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include "Usuario.h"
+#include "Subscriptor.h"
+#include "DTNotificacion.h"
+#include "Inmobiliaria.h"
+
+Inmobiliaria::Inmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email,
+            std::string direccion, std::string telefono, std::string url):
+    Usuario(nickname,contrasena,nombre,email),direccion(direccion),telefono(telefono),url(url){}
+
+void Inmobiliaria::agregarSubscriptor(Subscriptor *s){
+    suscriptores.insert(s);
+}
+
+void Inmobiliaria::eliminarSubscriptor(Subscriptor* s){
+    suscriptores.erase(s);
+}
+
+void Inmobiliaria::notificar(const DTNotificacion& n){
+    std::set<Subscriptor*>::iterator it;
+    for (it = suscriptores.begin(); it != suscriptores.end(); ++it){
+        (*it)->recibirNotificacion(n);
+    }
+}
+
+Inmobiliaria::~Inmobiliaria(){
+    suscriptores.clear();
+}
