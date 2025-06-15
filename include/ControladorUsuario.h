1d8bd7c6b6677d6b08207d9b1ce1321289645856
Nacho <ignerrecart@gmail.com>
2025-06-15 17:52:21 -0300

    Agregado al controlador

--- include/ControladorUsuario.h
+++ include/ControladorUsuario.h
@@ -20,6 +20,8 @@ public:
      bool altaCliente(std::string nickname, std::string contrasena, std::string nombre, std::string email,std::string apellido, std::string documento);
         bool altaInmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string direccion,std::string url, std::string telefono);
         bool altaPropietario(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string cuentaBancaria,std::string telefono);
+        void altaCasa(std::string direccion, std::int numeroPuerta, std::int superficie, std::int anoConstruccion, std::bool esPH, TipoTecho techo);
+        void altaApartamento(std::string direccion, std::int numeroPuerta, std::int superficie, std::int anoConstruccion, std::int piso, std::bool tieneAscensor, std::float gastosComunes);
         std::set<DTUsuario*> ListarInmobiliarias();
         std::set<DTUsuario*> ListarPropietarios();
         std::set<DTUsuario*> ListarClientes();
