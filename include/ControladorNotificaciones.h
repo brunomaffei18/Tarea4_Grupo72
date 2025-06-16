8aabffc60effdfd053d43df8370173f98225b7fd
Ariel Falcon <ariel.falcon.m@gmail.com>
2025-06-16 12:57:00 -0300

    Agrego ManejadorNotificaciones, termino los .h de Controlador y Manejador

--- include/ControladorNotificaciones.h
+++ include/ControladorNotificaciones.h
@@ -1,34 +1,31 @@
 #ifndef CONTROLADORNOTIFICACIONES_H
 #define CONTROLADORNOTIFICACIONES_H
 
-#include "Subscriptor.h"
-#include "Inmobiliaria.h"
-#include "DTNotificacion.h"
+#include "../include/Subscriptor.h"
+#include "../include/Inmobiliaria.h"
+#include "../include/DTNotificacion.h"
+#include "../include/ManejadorNotificaciones.h"
 #include <string>
 #include <map>
 #include <set>
 
 class ControladorNotificaciones {
 private:
-    std::map<std::string, Subscriptor*> subscriptores;
-
     ControladorNotificaciones();
     static ControladorNotificaciones* instancia;
+    ManejadorNotificaciones* manejador;
 
-public:     //Ta todo mal el uml :)
+public:
     static ControladorNotificaciones* getInstance();
 
-    void registrarSubscriptor(Subscriptor* s);
-    void registrarInmobiliaria(Inmobiliaria* i);    //Mi amigo
-
-    void suscribirse(std::string nickSub, std::string nickInmo);
-    void desuscribirse(std::string nickSub, std::string nickInmo);
-
-    std::set<DTNotificacion> consultarNotificaciones(std::string nickSub);
-    void limpiarNotificaciones(std::string nickSub);    //Mi amigo
-
-    void notificar(std::string nickInmo, const DTNotificacion& noti);    //Mi amigo
+    void subscribirse(const std::string& nickName, const std::list<std::string>& inmobiliarias);
+    std::set<DTNotificacion> consultarNotificaciones(const std::string& nickName);
+    void eliminarSubscripciones(const std::string& nickName, const std::list<std::string>& inmobiliarias);
+    std::list<std::string> nuevasSuscribciones(const std::string& nicknameInteresado);
+    void seSuscribe(const std::string& nicknameInmobiliaria, const std::string& nicknameInteresado);
+    std::list<std::string> listarInmobiliariasSuscriptas(const std::string& nicknameSubscriptor);
 
     ~ControladorNotificaciones();
 };
+
 #endif
---
+++ include/ManejadorNotificaciones.h
@@ -0,0 +1,34 @@
+// ManejadorNotificaciones.h
+#ifndef MANEJADORNOTIFICACIONES_H
+#define MANEJADORNOTIFICACIONES_H
+
+#include "../include/Subscriptor.h"
+#include "../include/Inmobiliaria.h"
+#include <string>
+#include <map>
+#include <list>
+
+class ManejadorNotificaciones {
+private:
+    std::map<std::string, Subscriptor*> subscriptores;
+    std::map<std::string, Inmobiliaria*> inmobiliarias;
+
+    static ManejadorNotificaciones* instancia;
+    ManejadorNotificaciones();
+
+public:
+    static ManejadorNotificaciones* getInstance();
+
+    void agregarSubscriptor(Subscriptor* s);
+    void agregarInmobiliaria(Inmobiliaria* i);
+
+    Subscriptor* obtenerSubscriptor(const std::string& nickname);
+    Inmobiliaria* obtenerInmobiliaria(const std::string& nickname);
+
+    std::list<std::string> obtenerNicksSubscriptos();
+    std::list<std::string> obtenerNicksInmobiliarias();
+
+    ~ManejadorNotificaciones();
+};
+
+#endif
