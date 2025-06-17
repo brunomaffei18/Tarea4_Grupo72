0cb374f204ac395eac6f43b9c65cb200dbf47658
Nacho <ignerrecart@gmail.com>
2025-06-17 11:27:46 -0300

    manejadorUsuario

--- include/ManejadorUsuario.h
+++ include/ManejadorUsuario.h
@@ -10,6 +10,9 @@
 class ManejadorUsuario {
     private:
         std::map<std::string, Usuario*> usuarios;
+        std::map<std::string, Cliente*> cleintes;
+        std::map<std::string, Propietario*> propietario;
+        std::map<std::string, Inmobiliaria*> Inmobiliaria;   
 
     public:
         void altaCliente(std::string nickname, std::string contrasena, std::string nombre, std::string email,std::string apellido, std::string documento);
