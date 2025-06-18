f33c21d33e95b1111938e4b2041ecd55ee400c0c
Martin <martinpedragosa730@gamil.com>
2025-06-17 21:45:17 -0300

    controladorPUblicacion litarpublicaciones

--- src/ControladorPublicacion.cpp
+++ src/ControladorPublicacion.cpp
@@ -6,6 +6,8 @@
 #include"../include/ManejadorUsuario.h"
 #include "../include/ManejadorInmueble.h"
 #include "../include/ControladorAdministrarInmueble.h"
+#include <set>
+
   ControladorPublicacion* ControladorPublicacion::getInstancia(){
     if (Instancia==NULL){
         Instancia=new ControladorPublicacion();
@@ -44,8 +46,29 @@
     
     
     };
+//     Se retorna el conjunto de los datavalue dtp del tipo DTPublicacion correspondientes a las instancias de Publicacion p que cumplen que 
+// p.tipo = tipoPublicacion, p.precio > precioMinimo, 
+// p.precio < precioMáximo y si tipoInmueble no es todos 
+// entonces el Inmueble relacionado a la instancia AdministraPropiadad ap 
+// relacionada a la Publicacion p es del tipo Casa si tipoInmueble = 
+// casa o Apartamento si tipoInmueble = apartamento. Tal que 
+// dtp.codigo = p.codigo, dtp.fecha = p.fecha, dtp.texto 
+// = p.texto, dtp.precio = p.precio y dtp.inmobiliaria = 
+// i.nombre donde i es la Inmobiliaria relacionada a AdministraPropiedad 
+// relacionada a la Publicación p
     std::set<DTPublicacion*> listarPublicaciones(TipoPublicacion tipoPublicacion, float precioMinimo, float precioMaximo, TipoInmueble tipoInmueble){
-     
+    std::set<DTPublicacion*> publicacionesFiltradas;
+    std::list<Publicacion*> listaPublicaciones = ManejadorPublicaciones::getManejadorPublicaciones()->listarPublicaciones();
+    for (std::list<Publicacion*>::iterator i = listaPublicaciones.begin(); i !=listaPublicaciones.end(); i++)
+    {
+      if ((*i)->getTipo()==tipoPublicacion && (*i)->getPrecio()>precioMinimo&& (*i)->getPrecio<precioMaximo
+      {
+        /* code */
+      }
+      
+    }
+    
+
     };
     DTPublicacion getPublicacion(int id){
 
