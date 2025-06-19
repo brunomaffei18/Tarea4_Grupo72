bb078b1009857cd335129f68641a1d146c459be6
Nacho <ignerrecart@gmail.com>
2025-06-19 12:46:05 -0300

    ManejadorUsuario

--- src/ManejadorUsuario.cpp
+++ src/ManejadorUsuario.cpp
@@ -13,5 +13,14 @@ ManejadorUsuario::ManejadorUsuario(){};
 bool ManejadorUsuario::existeUsuario(std::string nickname)
 { 
     return (usuarios.find(nickname) != usuarios.end());
-    
+}
+
+Inmobiliaria* getInmobiliaria(std::string nickname);
+{ 
+    inmo=inmobiliarias.find(nickname);
+    if(inmo!=inmobiliarias.end()){
+        return inmo->second;
+    }
+    else
+        return inmo ;
 }
