88dd02532c91fc492593b8564ad4590bd71edabf
Ariel Falcon <ariel.falcon.m@gmail.com>
2025-06-20 10:21:30 -0300

    CargaDatos.h

--- include/CargaDatos.h
+++ include/CargaDatos.h
@@ -1,10 +1,24 @@
 #ifndef CARGADATOS_H
 #define CARGADATOS_H
 
+#include "../include/IControladorUsuario.h"
+#include "../include/IControladorPublicacion.h"
+#include "../include/IControladorFechaActual.h"
+#include "../include/IControladorNotificaciones.h"
+#include "../include/IControladorInmueble.h"
+#include "../include/IControladorAdministrarInmueble.h"
+#include "../include/Factory.h"
 class CargaDatos {
     private:
         static CargaDatos* instance;
         CargaDatos();
+
+        IControladorUsuario* cu;
+        IControladorPublicacion* cp;
+        IControladorFechaActual* cf;
+        IControladorNotificaciones* cn;
+        IControladorInmueble* ci;
+        IControladorAdministrarInmueble* ca;
 
     public:
         static CargaDatos* getInstance();
