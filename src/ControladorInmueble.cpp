3c3a96ca5a7b5c1716c2210f29bda5438cac96d2
Ariel Falcon <ariel.falcon.m@gmail.com>
2025-06-20 12:55:03 -0300

    Error boludo

--- src/ControladorInmueble.cpp
+++ src/ControladorInmueble.cpp
@@ -54,7 +54,6 @@ void ControladorInmueble:: altaCasa(std::string direccion, int numeroPuerta, int
         if (this->propietarioRecordado != NULL)
         {
         this->propietarioRecordado->vincularInmueble(casanueva);
-        casanueva.
         }  
 };
 
